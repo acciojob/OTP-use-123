14492ba5ac4852ff346678ea05e6f56ea687cfb5
use-123 <anmolbejugam@gmail.com>
2024-12-24 17:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', () => {
+        if (input.value.length === 1 && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+
+      input.addEventListener('keydown', (event) => {
+        if (event.key === 'Backspace' && input.value.length === 0 && index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = ''; // Clear the previous input
+        }
+      });
+    });
+
+    function submitOtp() {
+      let otp = '';
+      inputs.forEach(input => otp += input.value);
+
+      if (otp.length === 6) {
+        alert(`OTP Submitted: ${otp}`);
+      } else {
+        alert('Please enter all 6 digits of the OTP');
+      }
+    }
